77750dd919f0d251ce082fe3aebe00cbb83354a4
dmitriy112233 <87652199+dmitriy112233@users.noreply.github.com>
2022-08-20 14:40:21 +0300

    Create Exchange Rates from NBU

---
+++ js/exchangeRates.js
@@ -0,0 +1,73 @@
+const url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
+
+const USD = 25;
+const EUR = 32;
+let courseUSD = 0;
+let courseEUR = 0;
+
+let leftInput = document.querySelector('#input-1');
+let rightInput = document.querySelector('#input-2');
+let leftSelect = document.querySelector('#left-select');
+let rightSelect = document.querySelector('#right-select');
+
+/* вывод нужных курсов на экран */
+
+async function exchangeRate(currency) {
+    let response = await fetch(url);
+    let json = await response.json();
+    let val = json[currency];
+
+    // меняем значение глобальных переменных courseUSD и courseEUR
+    if (currency === USD) courseUSD = +val.rate.toFixed(2);
+    if (currency === EUR) courseEUR = +val.rate.toFixed(2);
+
+    document.querySelector('.currency').outerHTML +=
+        `<p>${val.cc} (${val.txt}): ${val.rate.toFixed(2)}</p>`;
+}
+
+/* если поменялось значение слева, меняем значение справа и наоборот */
+
+function changeRightValue() {
+    rightInput.value = convert(leftInput.value, 'left').toFixed(2);
+}
+
+function changeLeftValue() {
+    leftInput.value = convert(rightInput.value, 'right').toFixed(2);
+}
+
+/* конвертация валют */
+
+function convert(num, position) {
+    let leftSelected = document.getElementById('left-select').selectedIndex;
+    let rightSelected = document.getElementById('right-select').selectedIndex;
+
+    // выбран UAH справа
+    if (rightSelected === 0) {
+        if (leftSelected === 0) return (position === 'left') ? num * courseUSD : num / courseUSD;
+        if (leftSelected === 1) return (position === 'left') ? num * courseEUR : num / courseEUR;
+        if (leftSelected === 2) return +num;
+    }
+
+    // выбран USD справа
+    if (rightSelected === 1) {
+        if (leftSelected === 0) return +num;
+        if (leftSelected === 1) return (position === 'left') ? courseEUR / courseUSD * num : courseUSD / courseEUR * num;
+        if (leftSelected === 2) return (position === 'left') ? num / courseUSD : num * courseUSD;
+    }
+
+    // выбран EUR справа
+    if (rightSelected === 2) {
+        if (leftSelected === 0) return (position === 'left') ? courseUSD / courseEUR * num : courseEUR / courseUSD * num;
+        if (leftSelected === 1) return +num;
+        if (leftSelected === 2) return (position === 'left') ? num / courseEUR : num * courseEUR;
+    }
+}
+
+exchangeRate(EUR);
+exchangeRate(USD);
+
+leftInput.oninput = changeRightValue;
+leftSelect.onchange = changeRightValue;
+
+rightInput.oninput = changeLeftValue;
+rightSelect.onchange = changeLeftValue;
